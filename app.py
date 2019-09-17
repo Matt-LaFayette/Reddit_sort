d9f73365a35fb029f74445db4db6055767b0e812
Matt LaFayette <lafayette.matt@gmail.com>
2019-09-17 11:59:20 -0400

    fix

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def get_results(offset=0, per_page=10):
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
- reddit = praw.Reddit(client_id=os.environ.get('client_id'),
+reddit = praw.Reddit(client_id=os.environ.get('client_id'),
                       client_secret=os.environ.get('client_secret'),
                       user_agent=os.environ.get('client_secret'),
                       username=os.environ.get('username'),
