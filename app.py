c939929ab00355664e3783ff7187e13d829eab16
Matt LaFayette <lafayette.matt@gmail.com>
2019-12-23 10:55:02 -0500

    heroku

--- app.py
+++ app.py
@@ -415,7 +415,7 @@ def index():
 
 @app.route('/createtable')
 def createtable():
-	sql = text('CREATE TABLE IF NOT EXISTS posts (id text primary key unique, title text, link text, category text, date_added int, thread_text TEXT, image text)')
+	sql = text('CREATE TABLE IF NOT EXISTS posts (id VARCHAR primary key unique, title VARCHAR, link VARCHAR, category VARCHAR, date_added VARCHAR, thread_text VARCHAR, image VARCHAR)')
 	db = create_engine('mysql://bf7c9445953141:59b50d69@us-cdbr-iron-east-05.cleardb.net/heroku_5196099a3078d00?reconnect=true')
 	db.execute(sql)
 	db.commit()
